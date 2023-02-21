f60741ab1a4a8b4e6d980aa06b1d2e04a7c797b9
DungNguyen2003 <91179133+DungNguyen2003@users.noreply.github.com>
2023-02-21 23:07:48 +0700

    feat/#6

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,9 +1,34 @@
 import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
+
+import HomeIcon from "@mui/icons-material/Home";
+import TimerIcon from "@mui/icons-material/Timer";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 import styles from "./Sidebar.module.scss";
 
 const cx = classNames.bind(styles);
 
 export const Sidebar = () => {
-  return <div className={cx("container")}>sidebar</div>;
+  return (
+    <div className={cx("container")}>
+      <Link to="" className={cx("button")}>
+        <HomeIcon />
+        <span>Trang chủ</span>
+      </Link>
+      <Link to="" className={cx("button")}>
+        <TimerIcon />
+        <span>Kiểm tra</span>
+      </Link>
+      <Link to="" className={cx("button")}>
+        <AssignmentIcon />
+        <span>Tài liệu</span>
+      </Link>
+      <Link to="" className={cx("button")}>
+        <FavoriteIcon />
+        <span>Yêu thích</span>
+      </Link>
+    </div>
+  );
 };
