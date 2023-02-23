2e959a813fbb30e23be9f69de6fc26f3df7df1fc
DungNguyen2003 <91179133+DungNguyen2003@users.noreply.github.com>
2023-02-23 16:44:22 +0700

    fix: feat/#6

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -7,13 +7,60 @@ import AssignmentIcon from "@mui/icons-material/Assignment";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
 import styles from "./Sidebar.module.scss";
+// import { MenuItem } from "@mui/material";
 
 const cx = classNames.bind(styles);
 
+const menuItem = [
+  {
+    path: "",
+    name: "Trang chủ",
+    icon: <HomeIcon />,
+  },
+  {
+    path: "",
+    name: "Kiểm tra",
+    icon: <TimerIcon />,
+  },
+  {
+    path: "",
+    name: "Tài liệu",
+    icon: <AssignmentIcon />,
+  },
+  {
+    path: "",
+    name: "Yêu thích",
+    icon: <FavoriteIcon />,
+  },
+];
 export const Sidebar = () => {
+  // const menuShow = (menuItems: any) => {
+  //   return menuItems.map((item: any, index: any) => {
+  //     // <a href={item.path} key={index} className={cx("button")}>
+  //     //   <div className={cx("icon")}>{item.icon}</div>
+  //     //   <div className={cx("name-element")}>{item.name}</div>
+  //     // </a>;
+  //     <div key={index}>{item.name}</div>;
+  //     console.log(item.name);
+  //   });
+  // };
+
   return (
     <div className={cx("container")}>
-      <Link to="" className={cx("button")}>
+      <div>
+        <button className="close_sidebar"></button>
+        <>
+          {menuItem.map((item, index) => {
+            <Link to={item.path} key={index} className={cx("button")}>
+              <div className={cx("icon")}>{item.icon}</div>
+              <div className={cx("name-element")}>{item.name}</div>
+            </Link>;
+            console.log(item.name);
+          })}
+        </>
+      </div>
+
+      {/* <Link to="" className={cx("button")}>
         <HomeIcon />
         <span>Trang chủ</span>
       </Link>
@@ -28,7 +75,7 @@ export const Sidebar = () => {
       <Link to="" className={cx("button")}>
         <FavoriteIcon />
         <span>Yêu thích</span>
-      </Link>
+      </Link> */}
     </div>
   );
 };
