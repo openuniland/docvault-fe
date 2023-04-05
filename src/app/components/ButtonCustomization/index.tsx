2ddaade96fe9c273ef562a13fc62fb22a24d1d60
traitimtrongvang <locxuandao260520033@gmail.com>
2023-04-05 18:01:48 +0700

    fix [feat/#58]

--- src/app/components/ButtonCustomization/index.tsx
+++ src/app/components/ButtonCustomization/index.tsx
@@ -1,7 +1,6 @@
 import classNames from "classnames/bind";
 import { ButtonBase } from "@mui/material";
 import AutorenewIcon from "@mui/icons-material/Autorenew";
-import { Link } from "react-router-dom";
 
 import styles from "./ButtonCustomization.module.scss";
 
@@ -12,19 +11,14 @@ interface Props {
   onClick?: () => void;
   className?: string;
   isLoading?: boolean;
-  page?: string;
-  prefix?: string;
-  id?: string;
 }
 
 export const ButtonCustomization = (props: Props) => {
-  const { children, onClick, className, isLoading, page, prefix, id } = props;
+  const { children, onClick, className, isLoading } = props;
 
   return (
-    <Link to={`/${page}/${prefix}/${id}`} className={cx("link")}>
-      <ButtonBase onClick={onClick} className={cx("button", className)}>
-        {isLoading ? <AutorenewIcon className={cx("loadingIcon")} /> : children}
-      </ButtonBase>
-    </Link>
+    <ButtonBase onClick={onClick} className={cx("button", className)}>
+      {isLoading ? <AutorenewIcon className={cx("loadingIcon")} /> : children}
+    </ButtonBase>
   );
 };
