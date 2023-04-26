d063443d42458c08937db41d60bd4625624e28f8
traitimtrongvang <locxuandao260520033@gmail.com>
2023-04-26 19:02:37 +0700

    fix [feat/#85] 3

--- src/app/containers/ProfilePageWrapper/index.tsx
+++ src/app/containers/ProfilePageWrapper/index.tsx
@@ -25,10 +25,9 @@ interface TabPanelProps {
 }
 
 const TabPanel = (props: TabPanelProps) => {
-  const { children, value, index, className, ...other } = props;
+  const { children, value, index, ...other } = props;
   return (
     <div
-      className={className}
       role="tabpanel"
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
