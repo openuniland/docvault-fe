2e93775816185882fdce554aef1ba9564567efdd
Loc Xuan Dao <102164071+locxuandao@users.noreply.github.com>
2023-04-30 23:10:55 +0700

    Feat/#85 (#86)
    * feat/#85
    
    * fix [feat/#85]
    
    * fix : [feat/#85]
    
    * fix [feat/#85] : [do loading animation]
    
    * fix [feat/#85] 3

--- src/services/userExam.ts
+++ src/services/userExam.ts
@@ -1,7 +1,9 @@
 import { AxiosResponse } from "axios";
-import { UserExam, CreateUserExamPayload } from "types/UserExam";
 
+import { UserExam, CreateUserExamPayload } from "types/UserExam";
 import http from "utils/api/http";
+import { URLparams, DataWithMeta } from "types";
+import { DEFAULT_PAGINATION } from "utils/constants";
 
 export const getUserExams = async (): Promise<UserExam[]> => {
   const response: AxiosResponse = await http.get(
@@ -15,4 +17,34 @@ export const createUserExam = async (
 ): Promise<UserExam> => {
   const response: AxiosResponse = await http.post(`/user-exams`, payload);
   return response?.data?.data;
+};
+
+export const getUserExamsCompletedByOwner = async (
+  urlParams: URLparams,
+): Promise<DataWithMeta<UserExam[]>> => {
+  const response: AxiosResponse = await http.get(
+    `/user-exams?is_completed=${true}`,
+    {
+      params: {
+        currentPage: urlParams?.currentPage || DEFAULT_PAGINATION.currentPage,
+        pageSize: urlParams?.pageSize || DEFAULT_PAGINATION.pageSize,
+      },
+    },
+  );
+  return response?.data;
+};
+
+export const getUserExamsNotCompletedByOwner = async (
+  urlParams: URLparams,
+): Promise<DataWithMeta<UserExam[]>> => {
+  const response: AxiosResponse = await http.get(
+    `/user-exams?is_completed=${false}`,
+    {
+      params: {
+        currentPage: urlParams?.currentPage || DEFAULT_PAGINATION.currentPage,
+        pageSize: urlParams?.pageSize || DEFAULT_PAGINATION.pageSize,
+      },
+    },
+  );
+  return response?.data;
 };
