4a6b7d917a133caf8b1b433ef2556ac8d7a1b556
nc-minh <nguyencongminh.official@gmail.com>
2023-05-07 18:40:02 +0700

    feat: [add tooltip]

--- src/app/components/ContentItem/index.tsx
+++ src/app/components/ContentItem/index.tsx
@@ -1,7 +1,7 @@
 import classNames from "classnames/bind";
 import { Link } from "react-router-dom";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
-import { Typography } from "@mui/material";
+import { Tooltip, Typography } from "@mui/material";
 import moment from "moment";
 
 import documentIcon from "assets/images/document.png";
@@ -24,31 +24,33 @@ export const ContentItem = (props: Props) => {
 
   return (
     <div className={cx("container")}>
-      <Link to={`/${prefix}/${id}`} className={cx("link")}>
-        <div className={cx("starWrapper")}>
-          <StarBorderIcon className={cx("starIcon")} />
-        </div>
-        <div className={cx("contentWrapper")}>
-          <img
-            src={isExam ? examIcon : documentIcon}
-            alt="book icon"
-            className={cx("image")}
-          />
-          <Typography variant="h2" component="h2" className={cx("title")}>
-            {title}
+      <Tooltip title={title} placement="top">
+        <Link to={`/${prefix}/${id}`} className={cx("link")}>
+          <div className={cx("starWrapper")}>
+            <StarBorderIcon className={cx("starIcon")} />
+          </div>
+          <div className={cx("contentWrapper")}>
+            <img
+              src={isExam ? examIcon : documentIcon}
+              alt="book icon"
+              className={cx("image")}
+            />
+            <Typography variant="h2" component="h2" className={cx("title")}>
+              {title}
+            </Typography>
+            <Typography
+              variant="caption"
+              component="strong"
+              className={cx("desc")}
+            >
+              {description}
+            </Typography>
+          </div>
+          <Typography variant="caption" component="time" className={cx("time")}>
+            {`${moment(createdAt).format("hh:mm - DD/MM/YYYY")}`}
           </Typography>
-          <Typography
-            variant="caption"
-            component="strong"
-            className={cx("desc")}
-          >
-            {description}
-          </Typography>
-        </div>
-        <Typography variant="caption" component="time" className={cx("time")}>
-          {`${moment(createdAt).format("hh:mm - DD/MM/YYYY")}`}
-        </Typography>
-      </Link>
+        </Link>
+      </Tooltip>
     </div>
   );
 };
